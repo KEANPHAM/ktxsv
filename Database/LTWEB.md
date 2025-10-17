8d4e3ebac82098e2b2ac8951f4571d0506124add
KEANPHAM <kienphamm.203@gmail.com>
2025-10-17 10:08:44 +0700

    update

---
+++ Database/LTWEB.md
@@ -0,0 +1,33 @@
+cos bao nhiêu loại trả về ở controller
+
+
+
+1. có bao nhiêu loại action trong controller
+2. trong action có các loại trả về view
+3. loại nào trả view loại nào trả api, chuỗi dữ lieu, object
+
+4\. có bao nhiêu loại Attribute/ công dung
+
+5\. các tham số có thể truyền vào
+
+6\. người dungfm muốn duyệt một trang với domain/Product/7 thì ta cầ action nào và không cần quyền truy cập
+
+
+
+1. các class có trong Html Helper
+2. tìm các câu lệnh của @HTML trong HTMLHelper
+3. cho thêm field số thứ tự vào code dưới
+4. Tạo một partial view là category list
+
+ <li>
+
+<category>
+
+<li>
+
+<li>
+
+nhúng vào menu trái
+
+@Html.Partial("NameView",ModelListcar)
+
--- Source Code/KTXSV/Controllers/PhongController.cs
+++ Source Code/KTXSV/Controllers/PhongController.cs
@@ -1,109 +1,120 @@
 ﻿using KTXSV.Models;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
-using static System.Collections.Specialized.BitVector32;
+
 namespace KTXSV.Controllers
 {
     public class PhongController : Controller
     {
         KTXSVEntities db = new KTXSVEntities();
 
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult HoSo()
+        {
+            return View();
+        }
         public ActionResult DangKyPhong(string gender, string building, int? capacity)
         {
-            //tạo session user mẫu để test
             Session["UserID"] = 1;
             Session["UserName"] = "Kiên Phạm";
             Session["Role"] = "Student";
-            //lấy dsach phòng trống
-            var phongTrong = db.Rooms.Where(phong => phong.Status == "Available").ToList();
-
-            if (!string.IsNullOrEmpty(gender) )
-            {
-                phongTrong = phongTrong.Where(p => p.Gender == gender).ToList();
-            }
-            if (!string.IsNullOrEmpty(building))
-            {
-                phongTrong = phongTrong.Where(p => p.Building == building).ToList();
-            }
-            if (capacity.HasValue)
-
-            {
-                phongTrong = phongTrong.Where(p => p.Capacity ==capacity.Value).ToList();
-            }
-            return View(phongTrong);
-        }
-
-        [HttpPost]
-        public ActionResult DangKyPhong(int roomId)
-        {
 
             int userId = int.Parse(Session["UserID"].ToString());
 
-            //Kiểm tra user đã đăng ký phòng chưa
-            bool kiemtra = db.Registrations.Any(r => r.UserID == userId && (r.Status == "Active" || r.Status == "Pending"));
-            if (kiemtra)
+            var dangKyHienTai = db.Registrations
+                .FirstOrDefault(r => r.UserID == userId && (r.Status == "Pending" || r.Status == "Active"));
+            ViewBag.DaDangKy = dangKyHienTai != null;
+
+            var phongTrong = db.Rooms.AsQueryable();
+
+            if (!string.IsNullOrEmpty(gender))
+                phongTrong = phongTrong.Where(p => p.Gender == gender);
+
+            if (!string.IsNullOrEmpty(building))
+                phongTrong = phongTrong.Where(p => p.Building == building);
+
+            if (capacity.HasValue)
+                phongTrong = phongTrong.Where(p => p.Capacity == capacity.Value);
+
+            return View(phongTrong.ToList());
+        }
+
+        // đăng ký phòng
+        [HttpPost]
+        public ActionResult DangKyPhong(int roomId)
+        {
+            int userId = int.Parse(Session["UserID"].ToString());
+
+            bool daDangKy = db.Registrations.Any(r => r.UserID == userId &&
+                (r.Status == "Active" || r.Status == "Pending"));
+            if (daDangKy)
             {
-                TempData["Error"] = "Bạn đã đăng ký phòng hoặc đang chờ duyệt.";
+                TempData["Error"] = "Bạn đã có phòng hoặc đang chờ duyệt. Không thể đăng ký thêm.";
                 return RedirectToAction("DangKyPhong");
             }
 
             var phong = db.Rooms.Find(roomId);
 
-            if (phong != null ||phong.Status == "Available" )
+            if (phong != null && phong.Status == "Available")
             {
-                var dangkymoi = new Registration
+                var dangKyMoi = new Registration
                 {
                     UserID = userId,
                     RoomID = roomId,
                     StartDate = DateTime.Now,
-                    Status = "Pending"
+                    Status = "Pending",
+                    
                 };
-                db.Registrations.Add(dangkymoi);
                 phong.Occupied = (phong.Occupied ?? 0) + 1;
-                if (phong.Occupied >= phong.Capacity)
-                {
-                    phong.Status = "Full";
-                }
+                db.Registrations.Add(dangKyMoi);
                 db.SaveChanges();
 
-                TempData["Success"]= "Đăng ký phòng thành công. Vui lòng chờ duyệt.";
+                TempData["Success"] = "Đăng ký phòng thành công. Vui lòng chờ duyệt.";
             }
             else
             {
-                TempData["Error"] = "Phong đã đầy.";
+                TempData["Error"] = "Phòng không tồn tại hoặc đã đầy.";
             }
+
             return RedirectToAction("DangKyPhong");
         }
+        //Phòng đã đk
+        public ActionResult DanhSachPhong()
+        {
+            int userId = int.Parse(Session["UserID"].ToString());
+
+            var dsDangKy = db.Registrations
+                .Where(r => r.UserID == userId)
+                .OrderByDescending(r => r.StartDate)
+                .ToList();
+
+            return View(dsDangKy);
+        }
+
         [HttpPost]
         public ActionResult HuyDangKy(int regId)
         {
             var reg = db.Registrations.Find(regId);
-
+            var phong = db.Rooms.Find(regId);
             if (reg != null && reg.Status == "Pending")
             {
-                db.Registrations.Remove(reg); 
+                db.Registrations.Remove(reg);
                 db.SaveChanges();
+                phong.Occupied = (phong.Occupied ?? 0) - 1;
 
-                TempData["Success"] = "Hủy yêu cầu đăng ký phòng thành công.";
+                TempData["Success"] = "Đã hủy yêu cầu đăng ký phòng.";
             }
             else
             {
-                TempData["Error"] = "Không thể hủy phòng đang duyệt hoặc đã active.";
+                TempData["Error"] = "Không thể hủy đăng ký đã duyệt hoặc không tồn tại.";
             }
 
-            return RedirectToAction("DanhSachDangKy"); 
-        }
-        public ActionResult DanhSachPhong()
-        {
-            return View();
-        }
-        public ActionResult Index()
-        {
-            return View();
+            return RedirectToAction("DanhSachPhong");
         }
     }
 }
-
