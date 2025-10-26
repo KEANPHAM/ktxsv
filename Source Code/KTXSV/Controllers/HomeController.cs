9420e518137b0889933c8fe5b68d3999e18ca2a1
khai <bin270106@gmail.com>
2025-10-26 15:17:31 +0700

    ghsh

--- Source Code/KTXSV/Controllers/HomeController.cs
+++ Source Code/KTXSV/Controllers/HomeController.cs
@@ -10,36 +10,7 @@ namespace KTXSV.Controllers
 
         public ActionResult Index()
         {
-            // 🧠 Giả lập đăng nhập (chỉ khi Session chưa có)
-            if (Session["UserID"] == null)
-            {
-                Session["UserID"] = 1;
-                Session["UserName"] = "Kiên Phạm";
-                Session["Role"] = "Student";
-            }
-
-            int userId = (int)Session["UserID"];
-            var role = db.Users
-                         .Where(u => u.UserID == userId)
-                         .Select(u => u.Role)
-                         .FirstOrDefault();
-
-            if (role == null)
-                return HttpNotFound("Không tìm thấy vai trò người dùng.");
-
-            string currentController = ControllerContext.RouteData.Values["controller"].ToString();
-
-            if (role == "Admin")
-            {
-                if (currentController != "Admin")
-                    return RedirectToAction("PendingRegistrations", "Admin");
-            }
-            else
-            {
-                if (currentController != "SupportRequests")
-                    return RedirectToAction("Index", "SupportRequests");
-            }
-
+           
             return View();
         }
     }
