a03a57b87de8c48c3e6f32e844b498d1668a7ae3
Ngoc Phi <gipphi147@gmail.com>
2025-10-30 22:50:19 +0700

    Add files via upload

---
+++ SinhVienController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KTXSV.Controllers
+{
+    public class SinhVienController : Controller
+    {
+        // GET: SinhVien
+        public ActionResult ChinhSuaThongTin()
+        {
+            return View();
+        }
+        public ActionResult ThongTinCaNhan()
+        {
+            return View();
+        }
+    }
+}
